b57a329414c783c2ab5492c3dda5d874f27a14ab
Iryna Stein <isophiestein@gmail.com>
2021-07-17 20:40:21 -0600

    commit tree fcd39dd8fe2408d4ff4847853c6bd7f86d0db40c

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,40 @@
 
+require 'pry'
+
+class Artist
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name= name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+        song
+    end
+
+    def genres
+        # songs.map(&:genre)
+        songs.collect {|song| song.genre}
+    end
+
+end
+
+# eminem = Artist.new("Eminem")
+# adele = Artist.new("Adele")
+# adele.new_song("Hello", "pop")
+# eminem.new_song("Slim Shady", "rap")
+# eminem.new_song("Love the way you lie", "rap")
+# eminem.songs
+
+# binding.pry
